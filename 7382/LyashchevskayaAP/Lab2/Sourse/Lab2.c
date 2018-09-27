35d7310a811a172e72bf030e912fd69bb0494f85
Лящевская Анастасия <31561368+Anastasiyalp@users.noreply.github.com>
2018-09-27 21:32:48 +0300

    Update Lab2.c

--- 7382/LyashchevskayaAP/Lab2/Sourse/Lab2.c
+++ 7382/LyashchevskayaAP/Lab2/Sourse/Lab2.c
@@ -8,19 +8,19 @@
 
 int main() {
 
-	char *str = (char *)malloc(100 * sizeof(char));
+	char *str = (char *)malloc(100 * sizeof(char));				//Allocate memory to string
 	fgets(str, 100, stdin);
-	if (!strcmp(str, "\n"))
+	if (!strcmp(str, "\n"))							//Cheking on the empty string
 		abort_("Expression is empty.");
 
-	printf("In first expr look like this %s", str);
+	printf("In first expr look like this %s", str);				//Output string
 
-	List list;
+	List list;								//Creating list
 	Node top;
-	list.top = &top;
-	fill_list(list.top, str);
+	list.top = &top;							//Creating node
+	fill_list(list.top, str);						//Fill list with our string
 	printf("Abridged expression lools like this:");
-	print_list(list.top);
+	print_list(list.top);							//Print abridged expression
 	printf("\n");
 	return 0;
 }
