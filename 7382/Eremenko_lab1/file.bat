a27f61038ce28b9765ed3503648ae06f025d04fc
annamenko <31561412+annamenko@users.noreply.github.com>
2018-09-27 13:26:16 +0300

    Add files via upload

---
+++ 7382/Eremenko_lab1/file.bat
@@ -0,0 +1,29 @@
+ECHO TEST 1: 
+MORE .\tests\test1.txt
+LAB < .\tests\test1.txt 
+ECHO TEST 2: 
+MORE .\tests\test2.txt
+LAB < .\tests\test2.txt
+ECHO TEST 3: 
+MORE .\tests\test3.txt
+LAB < .\tests\test3.txt 
+ECHO TEST 4: 
+MORE .\tests\test4.txt
+LAB < .\tests\test4.txt 
+ECHO TEST 5: 
+MORE .\tests\test5.txt
+LAB < .\tests\test5.txt 
+ECHO TEST 6: 
+MORE .\tests\test6.txt
+LAB < .\tests\test6.txt 
+ECHO TEST 7: 
+MORE .\tests\test7.txt
+LAB < .\tests\test7.txt 
+ECHO TEST 8: 
+MORE .\tests\test8.txt
+LAB < .\tests\test8.txt
+ECHO TEST 9: 
+MORE .\tests\test9.txt
+LAB < .\tests\test9.txt   
+DEL OUTPUT.TXT INPUT.TXT
+
